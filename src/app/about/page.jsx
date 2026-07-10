6b0d234ae136e7c34d0a846d79d29f6119243771
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-11 01:19:18 +0600

    Practising route

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            this is about About Page
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div className='text-green-500'>
+            let try another one
+        </div>
+    );
+};
+
+export default ContactPage;
