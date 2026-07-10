a022c958011f8e88edfc4258f4a196a10d375777
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-11 01:31:03 +0600

    practising nested route

---
+++ src/app/about/personal/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PersonalPage = () => {
+    return (
+        <div className='text-7xl'>
+            This is personal page
+        </div>
+    );
+};
+
+export default PersonalPage;
