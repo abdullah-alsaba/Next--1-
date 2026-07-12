66e08b7bc7e0ab5e258120f629615d01563d30e3
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-12 23:05:51 +0600

    add theme toggle button

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -1,4 +1,6 @@
 "use client";
+import { Check, Moon, Power, Sun } from "@gravity-ui/icons";
+import { Switch } from "@heroui/react";
 
 import React from "react";
 import { useTheme } from "next-themes";
@@ -7,9 +9,31 @@ const ThemeButton = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      Toggle {theme === "dark" ? "Light" : "Dark"} Mode
-    </button>
+    <Switch
+      aria-label="Power"
+      onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {({ isSelected }) => (
+        <Switch.Content>
+          <Switch.Control
+            className={`h-7.75 w-12.75 bg-blue-500 ${isSelected ? "bg-cyan-500 shadow-[0_0_12px_rgba(6,182,212,0.5)]" : ""}`}
+          >
+            <Switch.Thumb
+              className={`size-6.75 bg-white shadow-sm ${isSelected ? "ms-5.5 shadow-lg" : ""}`}
+            >
+              <Switch.Icon>
+                {isSelected ? (
+                  <Sun className="size-4 text-cyan-600" />
+                ) : (
+                  <Moon className="size-4 text-blue-600" />
+                )}
+              </Switch.Icon>
+            </Switch.Thumb>
+          </Switch.Control>
+        </Switch.Content>
+      )}
+    </Switch>
+   
   );
 };
 
