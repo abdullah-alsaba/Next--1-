c6e0905fbe5df3af6782d35d505150e07539d911
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-13 00:24:40 +0600

    update style

--- src/app/task/page.jsx
+++ src/app/task/page.jsx
@@ -8,17 +8,17 @@ const TaskPage = async () => {
 
     const tasks =await getTasks()
     return (
-        <div>
-            Tasks: {tasks.length}
-            <div className='mt-10 mb-10 flex justify-center items-center'>
-                <AddTask addATask={addATask}></AddTask>
-            </div>
-            <div className='grid grid-cols-3 gap-4'>
-                {
-                    tasks.map((task)=> <TaskCard key={task.id} task={task}></TaskCard>)
-                }
-            </div>
+      <div className=" mx-auto max-w-7xl">
+            <h1 className='text-4xl font-bold mt-6'>Tasks: {tasks.length}</h1>
+        <div className="mt-10 mb-10 flex justify-center items-center">
+          <AddTask addATask={addATask}></AddTask>
         </div>
+        <div className="grid grid-cols-3 gap-4">
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task}></TaskCard>
+          ))}
+        </div>
+      </div>
     );
 };
 
